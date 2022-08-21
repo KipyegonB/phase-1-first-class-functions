019af5c18ba7a5e87d6f4ab62cb466e4820590d4
KipyegonB <koechbernard08@gmail.com>
2022-08-21 12:15:14 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let receivesAFunction = (callBack) => {
+    callBack()
+}
+
+function returnsANamedFunction (){
+    return function ben(){
+}
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+}
